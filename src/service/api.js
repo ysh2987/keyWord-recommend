3c05a41dd7cbe9e9910751335fe147a6cdc54787
ysh2987 <ysh2987@naver.com>
2022-03-26 02:09:48 +0900

    deploy

--- src/service/api.js
+++ src/service/api.js
@@ -3,8 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl:
-      'https://api.clinicaltrialskorea.com/api/v1/search-conditions/?name=',
+    baseUrl: process.env.REACT_APP_API_URL,
   }),
   keepUnusedDataFor: 15,
   endpoints: (builder) => ({
